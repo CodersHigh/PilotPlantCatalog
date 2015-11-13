0a6b32ac37c642bd3524f80d93677e677d650d40
Seongkwan Yoon <admin@lingostar.co.kr>
2015-11-13 15:48:28 +0900

    InitialCommit

---
+++ PilotPlant.framework/Headers/PilotPlant.h
@@ -0,0 +1,19 @@
+//
+//  PilotPlant.h
+//  PilotPlant
+//
+//  Created by Lingostar on 2015. 11. 4..
+//  Copyright © 2015년 LingoStar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PilotPlant.
+FOUNDATION_EXPORT double PilotPlantVersionNumber;
+
+//! Project version string for PilotPlant.
+FOUNDATION_EXPORT const unsigned char PilotPlantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PilotPlant/PublicHeader.h>
+
+
